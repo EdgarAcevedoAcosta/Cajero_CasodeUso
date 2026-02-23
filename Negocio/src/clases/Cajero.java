159d76f30e3b596e226d106ca4d4d92a1e1952a7
EdgarAcevedoAcosta <edgar.acevedo245769@potros.itson.edu.mx>
2026-02-23 12:46:17 -0700

    negocio

---
+++ Negocio/src/clases/Cajero.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author edgar
+ */
+public class Cajero {
+    private int idCajero;
+    public double cantidadDinero;
+    private Usuario usuario;
+    private Tarjeta tarjeta;
+
+    public Cajero(int idCajero, double cantidadDinero) {
+        this.idCajero = idCajero;
+        this.cantidadDinero = cantidadDinero;
+    }
+
+    public int getIdCajero() {
+        return idCajero;
+    }
+
+    public void setIdCajero(int idCajero) {
+        this.idCajero = idCajero;
+    }
+
+    public double getCantidadDinero() {
+        return cantidadDinero;
+    }
+
+    public void setCantidadDinero(double cantidadDinero) {
+        this.cantidadDinero = cantidadDinero;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Tarjeta getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+    
+    
+}
